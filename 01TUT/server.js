a777795fca93199042f70e0d4fe06c4b0cc56e55
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-01 06:02:13 +0530

    basics

--- 01TUT/server.js
+++ 01TUT/server.js
@@ -1,4 +1,16 @@
 const os = require("os");
+const path = require("path");
+const math = require("./math");
+console.log(math.addition(2, 5));
+
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
