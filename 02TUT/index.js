e32da69a38edd7f1508ec4c875c6f36fda47df97
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-02 15:04:27 +0530

    fs done

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -1,35 +1,46 @@
 const fs = require("fs");
 const path = require("path");
-fs.readFile("./files/starter.txt", "utf-8", (err, data) => {
-  // fs.readFile(
-  //   path.join(__dirname, "files", "starter.text"),
-  //   "utf-8",
-  //   (err, data) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log(data.toString());
+
+fs.readFile(
+  path.join(__dirname, "files", "starter.txt"),
+  "utf-8",
+  (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(data);
+    }
   }
-});
+);
 
-console.log("hello.");
+console.log("hello");
 
-fs.writeFile("./files/reply.txt", "Nice to meet you .", (err) => {
-  if (err) {
-    throw err;
-  } else {
+//big callback hell,to escape use promises
+fs.writeFile(
+  path.join(__dirname, "files", "reply.txt"),
+  "Nice to meet you ",
+  (err) => {
+    if (err) throw err;
     console.log("write complete");
-  }
-});
 
-fs.writeFile("./files/test.txt", "Testing text .", (err) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log("append complete");
+    fs.appendFile(
+      path.join(__dirname, "files", "reply.txt"),
+      "\n\n Yes it is .",
+      (err) => {
+        if (err) throw err;
+        console.log("Appended");
+        fs.rename(
+          path.join(__dirname, "files", "reply.txt"),
+          path.join(__dirname, "files", "newReply.txt"),
+          (err) => {
+            if (err) throw err;
+            console.log("renamed");
+          }
+        );
+      }
+    );
   }
-});
-
+);
 process.on("uncaughtException", (err) => {
   console.log(`There was an uncaught error : ${err}`);
   process.exit(1);
