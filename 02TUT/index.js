fd6682c90d0e2d48538882d26da232cda2690c35
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-02 12:22:22 +0530

    about fs

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -1,9 +1,36 @@
 const fs = require("fs");
-
+const path = require("path");
 fs.readFile("./files/starter.txt", "utf-8", (err, data) => {
+  // fs.readFile(
+  //   path.join(__dirname, "files", "starter.text"),
+  //   "utf-8",
+  //   (err, data) => {
   if (err) {
     throw err;
   } else {
     console.log(data.toString());
   }
+});
+
+console.log("hello.");
+
+fs.writeFile("./files/reply.txt", "Nice to meet you .", (err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("write complete");
+  }
+});
+
+fs.writeFile("./files/test.txt", "Testing text .", (err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("append complete");
+  }
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(`There was an uncaught error : ${err}`);
+  process.exit(1);
 });
