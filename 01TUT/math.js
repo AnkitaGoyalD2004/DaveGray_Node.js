28f3265b7e231b80ec95e519641be08ba267658b
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-07 06:04:21 +0530

    new chapter begans

--- 01TUT/math.js
+++ 01TUT/math.js
@@ -1,6 +1,6 @@
-const addition = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a - b;
-const divide = (a, b) => a - b;
+exports.addition = (a, b) => a + b;
+exports.subtract = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.divide = (a, b) => a / b;
 
-module.exports = { addition, subtract, multiply, divide };
+// module.exports = { addition, subtract, multiply, divide };
--- 01TUT/server.js
+++ 01TUT/server.js
@@ -1,7 +1,16 @@
 const os = require("os");
 const path = require("path");
+
+// 5 and 6 line are same
 const math = require("./math");
+// const { addition } = require("./math");
+const { addition, subtract, divide, multiply } = require("./math");
+
 console.log(math.addition(2, 5));
+console.log(addition(3, 5));
+console.log(subtract(3, 5));
+console.log(divide(3, 5));
+console.log(multiply(3, 5));
 
 console.log(os.type());
 console.log(os.version());
