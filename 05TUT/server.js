8b6672d630dcbdcbb160da5b6962c907c1c07974
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-09 12:32:24 +0530

    done

---
+++ 05TUT/server.js
@@ -0,0 +1,13 @@
+const logEvents = require("./logEvents");
+
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+// initialize the object
+const myEmitter = new MyEmitter();
+// add listner or the log event
+myEmitter.on("logs", (msg) => logEvents(msg));
+setTimeout(() => {
+  //Emit events
+  myEmitter.emit("logs", "log event emitted");
+}, 5000);
