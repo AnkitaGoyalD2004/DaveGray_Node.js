fb4504fcde5074d754bef74a87e8fc655a0ae6af
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-10 08:51:15 +0530

    done

--- 05TUT/server.js
+++ 05TUT/server.js
@@ -1,13 +1,24 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require(fs).promises;
+
 const logEvents = require("./logEvents");
-
 const EventEmitter = require("events");
-
-class MyEmitter extends EventEmitter {}
+class Emitter extends EventEmitter {}
 // initialize the object
-const myEmitter = new MyEmitter();
-// add listner or the log event
-myEmitter.on("logs", (msg) => logEvents(msg));
-setTimeout(() => {
-  //Emit events
-  myEmitter.emit("logs", "log event emitted");
-}, 5000);
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+});
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+// // add listner or the log event
+// myEmitter.on("logs", (msg) => logEvents(msg));
+// setTimeout(() => {
+//   //Emit events
+//   myEmitter.emit("logs", "log event emitted");
+// }, 5000);
