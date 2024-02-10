077e05aeaed079a46642de9e0c4605583807e859
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-10 09:28:41 +0530

    basic http server

--- 05TUT/server.js
+++ 05TUT/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const fsPromises = require(fs).promises;
+const fsPromises = require("fs").promises;
 
 const logEvents = require("./logEvents");
 const EventEmitter = require("events");
@@ -13,6 +13,60 @@ const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // let path;
+  // switch (req.url) {
+  //   case "/":
+  //     res.statusCode = 200;
+  //     res.setHeader("Content-Type", "text/html");
+  //     path = path.join(__dirname, "views", " index.html");
+  //     fs.readFile(path, "utf-8", (err, data) => {
+  //       res.end(data);
+  //     });
+  //     break;
+  // }
+
+  const extension = path.extname(req.url);
+  let contentType;
+  switch (extension) {
+    case ".css":
+      contentType = "text/css";
+      break;
+
+    case ".js":
+      contentType = "text/javascript";
+      break;
+
+    case ".json":
+      contentType = "application/json";
+      break;
+
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+
+    case ".png":
+      contentType = "image/png";
+      break;
+
+    case ".txt":
+      contentType = "text/plain";
+      break;
+
+    default:
+      contentType = "text/html";
+  }
+
+  let filePath =
+    contentType === "text/html" && req.url === "/"
+      ? path.join(__dirname, "views", "index.html")
+      : contentType === "text/html" && req.url.slice(-1) === "/"
+      ? path.join(__dirname, "views", req.url, "index.html")
+      : contentType === "text/html"
+      ? path.join(__dirname, "views", req.url)
+      : path.join(__dirname, req.url);
+
+  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
 });
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
