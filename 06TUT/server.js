6847e8ed2dc47e942478447098164db02ae35ee0
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-20 15:00:53 +0530

    basic

--- 06TUT/server.js
+++ 06TUT/server.js
@@ -11,5 +11,40 @@ app.get("^/$|/index(.html)?", (req, res) => {
 app.get("/new-page(.html)", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
+
+//Route Handler
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
+
+//chaining the route handler
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+const three = (req, res) => {
+  console.log("three");
+  res.end("Finished");
+};
+
+app.get("/chain(.html)?", [one, two, three]);
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
