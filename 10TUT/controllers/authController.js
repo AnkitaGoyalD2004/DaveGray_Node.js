3e5d18e3bbe1bf903a7dcb7ef5acd48364a39424
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-23 14:39:22 +0530

    auth

--- 10TUT/controllers/authController.js
+++ 10TUT/controllers/authController.js
@@ -4,3 +4,23 @@ const userDB = {
     this.users = data;
   },
 };
+const bcrypt = require("bcrypt");
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: `Username ans password are required.` });
+  const foundUser = usersDB.users.find(person.username === user);
+  if (!foundUser) return res.sendStatus(401); // unauthorized
+  // evaluate password
+  const match = await bcrypt.compare(pwd, foundUser.password);
+  if (match) {
+    //create JWT
+    res.json({ success: `User ${user} is logged in !` });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
