e0ee6b3164dd90fb5d160280f68c9b767431108b
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-26 16:22:50 +0530

    about JWT

--- 11TUT/controllers/authController.js
+++ 11TUT/controllers/authController.js
@@ -1,3 +1,65 @@
+// //emulating a db
+// const usersDb = {
+//   users: require("../data/users.json"),
+//   setUsers: function (data) {
+//     this.users = data;
+//     return;
+//   },
+// };
+
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// const fsPromises = require("fs").promises;
+// const path = require("path");
+
+// const handleLogin = async (req, res) => {
+//   const { user, pwd } = req.body;
+//   if (!user || !pwd) {
+//     return res
+//       .status(400)
+//       .json({ message: "usename and password are required" });
+//   }
+//   //find the user
+//   const foundUser = usersDb.users.find((person) => {
+//     return person.username == user;
+//   });
+//   if (!foundUser) return res.status(401).json({ message: "User not found" }); //unauthorized
+//   const match = await bcrypt.compare(pwd, foundUser.password);
+//   if (match) {
+//     //create jwts
+//     const accessToken = jwt.sign(
+//       { username: foundUser.username },
+//       process.env.ACCESS_TOKEN_SECRET,
+//       { expiresIn: "30s" }
+//     );
+//     const refreshToken = jwt.sign(
+//       { username: foundUser.username },
+//       process.env.REFRESH_TOKEN_SECRET,
+//       { expiresIn: "1d" }
+//     );
+//     //Saving refreshToken with current user
+//     const otherUsers = usersDb.users.flter(
+//       (person) => personUsername !== foundUser.username
+//     );
+//     const currentUser = { ...foundUser, refreshToken };
+//     usersDb.setUsers([...otherUsers, currentUser]);
+//     await fsPromises.writeFile(
+//       path.join(__dirname, "..", "data", "users.json"),
+//       JSON.stringify(usersDb.users)
+//     );
+//     res.cookie("jwt", refreshToken, {
+//       httpOnly: true,
+//       maxAge: 24 * 60 * 60 * 1000,
+//     });
+//     return res.json({ accessToken });
+//   } else {
+//     return res.sendStatus(401);
+//   }
+// };
+
+// module.exports = { handleLogin };
+
 //emulating a db
 const usersDb = {
   users: require("../data/users.json"),
@@ -8,8 +70,14 @@ const usersDb = {
 };
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const path = require("path");
+//To emulate a db
+const fsPromises = require("fs").promises;
 
 const handleLogin = async (req, res) => {
+  console.log("Hello from login");
   const { user, pwd } = req.body;
   if (!user || !pwd) {
     return res
@@ -21,28 +89,40 @@ const handleLogin = async (req, res) => {
     return person.username == user;
   });
   if (!foundUser) return res.status(401).json({ message: "User not found" });
-  //evaluate password;
-  //console.log(pwd,foundUser.password);
-  //console.log(user,foundUser);
-  bcrypt.compare(pwd, foundUser.password, (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.sendStatus(500);
-    }
-    if (result) {
-      return res.json({ message: `User ${user} is logged in` });
-    } else {
-      return res.sendStatus(401);
-    }
-  });
+  //evaluate password
+  const match = await bcrypt.compare(pwd, foundUser.password);
 
-  // console.log(foundUser);
-  // const match = await bcrypt.compare(pwd,foundUser.password);
-  // if(match){
-  //     return res.json({"message":`User ${user} is logged in`});
-  // }else{
-  //     return res.sendStatus(401);
-  // }
+  if (match) {
+    //create a jwt
+    //we will not use the password as the payload , will hurt the security
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "50s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    //saving refresh token with the current user
+    const otherUsers = usersDb.users.filter((person) => {
+      return person.username != foundUser.username;
+    });
+    const currentUser = { ...foundUser, refreshToken };
+    usersDb.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "models", "users.json"),
+      JSON.stringify(usersDb.users)
+    );
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    return res.json({ accessToken });
+  } else {
+    return res.sendStatus(401);
+  }
 };
 
 module.exports = { handleLogin };
