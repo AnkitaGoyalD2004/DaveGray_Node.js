636f79d4f6e4b48f9ad1d80a08d283c42b234558
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-02-27 10:27:05 +0530

    jwt

--- 11TUT/controllers/authController.js
+++ 11TUT/controllers/authController.js
@@ -1,65 +1,3 @@
-// //emulating a db
-// const usersDb = {
-//   users: require("../data/users.json"),
-//   setUsers: function (data) {
-//     this.users = data;
-//     return;
-//   },
-// };
-
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-// require("dotenv").config();
-// const fsPromises = require("fs").promises;
-// const path = require("path");
-
-// const handleLogin = async (req, res) => {
-//   const { user, pwd } = req.body;
-//   if (!user || !pwd) {
-//     return res
-//       .status(400)
-//       .json({ message: "usename and password are required" });
-//   }
-//   //find the user
-//   const foundUser = usersDb.users.find((person) => {
-//     return person.username == user;
-//   });
-//   if (!foundUser) return res.status(401).json({ message: "User not found" }); //unauthorized
-//   const match = await bcrypt.compare(pwd, foundUser.password);
-//   if (match) {
-//     //create jwts
-//     const accessToken = jwt.sign(
-//       { username: foundUser.username },
-//       process.env.ACCESS_TOKEN_SECRET,
-//       { expiresIn: "30s" }
-//     );
-//     const refreshToken = jwt.sign(
-//       { username: foundUser.username },
-//       process.env.REFRESH_TOKEN_SECRET,
-//       { expiresIn: "1d" }
-//     );
-//     //Saving refreshToken with current user
-//     const otherUsers = usersDb.users.flter(
-//       (person) => personUsername !== foundUser.username
-//     );
-//     const currentUser = { ...foundUser, refreshToken };
-//     usersDb.setUsers([...otherUsers, currentUser]);
-//     await fsPromises.writeFile(
-//       path.join(__dirname, "..", "data", "users.json"),
-//       JSON.stringify(usersDb.users)
-//     );
-//     res.cookie("jwt", refreshToken, {
-//       httpOnly: true,
-//       maxAge: 24 * 60 * 60 * 1000,
-//     });
-//     return res.json({ accessToken });
-//   } else {
-//     return res.sendStatus(401);
-//   }
-// };
-
-// module.exports = { handleLogin };
-
 //emulating a db
 const usersDb = {
   users: require("../data/users.json"),
@@ -124,4 +62,5 @@ const handleLogin = async (req, res) => {
     return res.sendStatus(401);
   }
 };
+
 module.exports = { handleLogin };
