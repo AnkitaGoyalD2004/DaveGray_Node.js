e353a838890c0601b189da6a2ddca9daf02625c4
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-01 06:34:50 +0530

    completed

--- 11TUT/controllers/refreshTokenController.js
+++ 11TUT/controllers/refreshTokenController.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const handleRefreshToken = (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) {
-    return res.status(401);
+    return res.sendStatus(401);
   }
   console.log(cookies.jwt);
   const refreshToken = cookies.jwt;
