a89cdae71f874c443835fdab6febfd6f9c52369a
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-01 06:53:04 +0530

    completed

--- 11TUT/server.js
+++ 11TUT/server.js
@@ -7,10 +7,15 @@ const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
 const PORT = process.env.PORT || 3500;
 
 //Cross Origin Resource Sharing
 app.use(cors(corsOptions));
+
+//Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
 
 //custom middleware logger(wil l be used everwhere as a middleware)
 app.use(logger);
