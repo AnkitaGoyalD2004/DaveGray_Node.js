a6e07626d9cd62a52431939d3709af801eab7e94
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-05 09:43:49 +0530

    user roles

--- 12TUT/controllers/authController.js
+++ 12TUT/controllers/authController.js
@@ -28,9 +28,15 @@ const handleLogin = async (req, res) => {
   const match = await bcrypt.compare(pwd, foundUser.password);
 
   if (match) {
+    const roles = Object.values(foundUser.roles);
     //create a jwt
     const accessToken = jwt.sign(
-      { username: foundUser.username },
+      {
+        UserInfo: {
+          username: foundUser.username,
+          roles: roles,
+        },
+      },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "50s" }
     );
