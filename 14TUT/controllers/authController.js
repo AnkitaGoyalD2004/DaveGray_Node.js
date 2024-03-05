911d5a2ec13448e43ed43aa4dc28cabcfa904181
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-05 14:42:08 +0530

    Mongoose

---
+++ 14TUT/controllers/authController.js
@@ -0,0 +1,70 @@
+//emulating a db
+const usersDb = {
+  users: require("../data/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const path = require("path");
+const fsPromises = require("fs").promises;
+
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd) {
+    return res
+      .status(400)
+      .json({ message: "usename and password are required" });
+  }
+  //find the user
+  const foundUser = usersDb.users.find((person) => {
+    return person.username == user;
+  });
+  if (!foundUser) return res.status(401).json({ message: "User not found" });
+  //evaluate password
+  const match = await bcrypt.compare(pwd, foundUser.password);
+
+  if (match) {
+    const roles = Object.values(foundUser.roles);
+    //create a jwt
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          username: foundUser.username,
+          roles: roles,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "50s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    //saving refresh token with the current user
+    const otherUsers = usersDb.users.filter((person) => {
+      return person.username != foundUser.username;
+    });
+    const currentUser = { ...foundUser, refreshToken };
+    usersDb.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "data", "users.json"),
+      JSON.stringify(usersDb.users)
+    );
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    return res.json({ accessToken });
+  } else {
+    return res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
