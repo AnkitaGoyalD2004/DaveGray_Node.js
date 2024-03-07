63be992f8fbea9efc383becbf5210448f28cd4ad
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-07 06:27:30 +0530

    about registration form

---
+++ ReactForm/vite-project/src/App.js
@@ -0,0 +1,10 @@
+import Register from "./Register";
+
+function App() {
+  return (
+    <main className="App">
+      <Register />
+    </main>
+  );
+}
+export default App;
---
+++ ReactForm/vite-project/src/Register.js
@@ -0,0 +1,46 @@
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[a-zA][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setVAlidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    useRef.current.focus();
+  }, []);
+
+  // this useEffect for the username
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  return <div></div>;
+};
+
+export default Register;
