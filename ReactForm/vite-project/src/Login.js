8f4466126b1ce2dafd95b33cd4c12e13c1d1c13a
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-09 16:11:18 +0530

    login setup

--- ReactForm/vite-project/src/Login.js
+++ ReactForm/vite-project/src/Login.js
@@ -1,4 +1,59 @@
+import { faGripLinesVertical } from "@fortawesome/free-solid-svg-icons";
+import { useRef, useState, useEffect } from "react";
 const Login = () => {
-  return <div></div>;
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setUser(e.target.value)}
+          value={pwd}
+          required
+        />
+        <button>Sign In</button>
+      </form>
+      <p>
+        Need an Account? <br />
+        <span className="line">
+          <a href="#">Sign Up</a>
+        </span>
+      </p>
+    </section>
+  );
 };
 export default Login;
