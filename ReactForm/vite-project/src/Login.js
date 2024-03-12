0ae976769ad41c4c7f1970dff4b7cf82b5a77de6
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-12 09:44:40 +0530

    Login page done

--- ReactForm/vite-project/src/Login.js
+++ ReactForm/vite-project/src/Login.js
@@ -1,23 +1,18 @@
-import { useRef, useState, useEffect } from "react";
-import useAuth from "../hooks/useAuth";
-import customFetch from "../api/axios";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "./context/AuthProvider";
+
+import axios from "./api/axios";
+const LOGIN_URL = "/auth";
 
 const Login = () => {
-  const { setAuth, persist, setPersist } = useAuth();
-
-  const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/";
-  //the above stores from where the user has arrived to the login form (can be a redirect)
-
+  const { setAuth } = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
 
-  //declare all the states
   const [user, setUser] = useState("");
   const [pwd, setPwd] = useState("");
   const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     userRef.current.focus();
@@ -27,32 +22,32 @@ const Login = () => {
     setErrMsg("");
   }, [user, pwd]);
 
-  //function top handle form submit
-  let handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
     try {
-      const response = await customFetch.post(
-        "/auth",
+      const response = await axios.post(
+        LOGIN_URL,
         JSON.stringify({ user, pwd }),
         {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         }
       );
-      //for obvious reasons ,remove the line below
       console.log(JSON.stringify(response?.data));
+      //console.log(JSON.stringify(response));
       const accessToken = response?.data?.accessToken;
       const roles = response?.data?.roles;
       setAuth({ user, pwd, roles, accessToken });
       setUser("");
       setPwd("");
-      navigate(from, { replace: true }); //replace:true, because now the user will not naviagte back to the login page again
-    } catch (error) {
-      if (!error?.response) {
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
         setErrMsg("No Server Response");
-      } else if (error?.response?.status == 400) {
-        setErrMsg("Missing Username or password");
-      } else if (error?.response?.status == 401) {
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
         setErrMsg("Unauthorized");
       } else {
         setErrMsg("Login Failed");
@@ -61,64 +56,59 @@ const Login = () => {
     }
   };
 
-  const togglePersist = () => {
-    setPersist((prev) => {
-      return !prev;
-    });
-  };
-
-  useEffect(() => {
-    localStorage.setItem("persist", persist);
-  }, [persist]);
-
   return (
-    <section>
-      <p
-        ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
-      <h1> Sign In</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          ref={userRef}
-          autoComplete="off"
-          onChange={(event) => setUser(event.target.value)}
-          value={user}
-          required
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          onChange={(event) => setPwd(event.target.value)}
-          value={pwd}
-          required
-        />
-        <button>Sign In</button>
-        <div className="persistCheck">
-          <input
-            type="checkbox"
-            id="persist"
-            onChange={togglePersist}
-            checked={persist}
-          ></input>
-          <label htmlFor="persist">Trust This Device</label>
-        </div>
-      </form>
-      <p>
-        Do not have a account?
-        <br />
-        <span className="line">
-          <Link to="/register">Sign Up</Link>
-        </span>
-      </p>
-    </section>
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+          <p>
+            Need an Account?
+            <br />
+            <span className="line">
+              {/*put router link here*/}
+              <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
   );
 };
 
