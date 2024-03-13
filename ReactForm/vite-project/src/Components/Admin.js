906864640e65d4ad542b05346f21ed7267fb1f11
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-13 06:07:16 +0530

    done

--- ReactForm/vite-project/src/Components/Admin.js
+++ ReactForm/vite-project/src/Components/Admin.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
-
+import Users from "./Users";
 const Admin = () => {
   return (
     <section>
       <h1>Admin Page</h1>
       <br />
-      <P>You must have been assigned an admin role.</P>
+      <Users />
+      <br />
       <div className="flexGrow">
         <Link to="/">Home</Link>
       </div>
